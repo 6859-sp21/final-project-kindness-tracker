a0963709d1c9945e209c5fed7463c16fc615c3f3
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2021-04-18 17:48:29 -0400

    Add Google Sheets loading to App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import Tabletop from 'tabletop'
 
-function App() {
+import './App.css'
+
+const App = () => {
+  const [data, setData] = useState(null)
+  const loading = data === null
+
+  useEffect(() => {
+    if (data === null) {
+      Tabletop.init({
+        // TODO add this key to some environment variable, don't hard-code here
+        key: 'https://docs.google.com/spreadsheets/d/1IqEBIcnFZ_BFCrD8jhk11yVoOjfxzZpiZLdL4cL3oK0/pubhtml',
+        simpleSheet: true,
+      })
+        .then(data => {
+          setData(data)
+        })
+        .catch(console.warn)
+    }
+  }, [data])
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Welcome to Kindness Tracker!
         </p>
+        <p>{loading ? 'Loading...' : `Loaded ${data.length} rows from Google Sheets!`}</p>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App
