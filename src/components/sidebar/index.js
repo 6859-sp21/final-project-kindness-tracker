f57f0ca414c74ff8a9b872a5c286bc04adef56cc
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2021-04-27 02:01:35 -0400

    Add bottom content in sidebar

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -46,6 +46,15 @@ const Sidebar = ({ isLoading, selectedNode, setSelectedNode, isTracing, setIsTra
                 traceIndex={traceIndex}
                 setTraceIndex={setTraceIndex}
             />
+            <div className="sidebar-bottom-content">
+                <p className="sidebar-small-text">Christian Moroney, Jackson Bernatchez, Kevin Lyons</p>
+                <p className="sidebar-small-text">6.859 Final Project Spring 2021</p>
+                <div>
+                    <a href="https://github.com/6859-sp21/final-project-kindness-tracker" target="_blank">
+                        <img className="sidebar-git-image" src="https://image.flaticon.com/icons/png/512/25/25231.png"></img>
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
